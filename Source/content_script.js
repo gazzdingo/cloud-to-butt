99e8d855fe1fb0606def1bad5c9b1f2e5d1fc34c
Guy Langford-Lee <fwg4379@aut.ac.nz>
2015-08-18 13:34:06 +1200

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,11 +31,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bThe\b/g, "the butt and the");
+	v = v.replace(/\bthe\b/g, "the butt and the");
+
 	textNode.nodeValue = v;
 }
 
